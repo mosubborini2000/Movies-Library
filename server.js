c3fce818b3df20673bd1b346ffe4b776be1da2fd
mosubborini2000 <mosubborini2000@gmail.com>
2023-07-08 18:36:12 +0000

    lab15

--- server.js
+++ server.js
@@ -17,6 +17,33 @@ const serError=require("./errorHandler/500");
 
 app.use(moviesRoutes);
 app.use(generalRoutes);
+
+app.get("/trending", async (req, res) => {
+  const axiosResponse = await axios.get(`https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.SECRET_API}&language=en-US`);
+  const myData = axiosResponse.data.results.map((result) => ({
+    "id": result.id,
+    "title": result.title,
+    "release_date": result.release_date,
+    "poster_path": result.poster_path,
+    "overview": result.overview
+  })); ;
+res.send(myData);
+});
+
+app.get("/search", async (req, res) => {
+  let mName = req.query.query; 
+ let axiosResponse = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.SECRET_API}&language=en-US&query=${mName}&page=2`);
+  const receveData = axiosResponse.data.results;
+  const movies = receveData.map((result) => ({
+    "id": result.id,
+    "title": result.title,
+    "release_date": result.release_date,
+    "poster_path": result.poster_path,
+    "overview": result.overview
+  }));
+  res.send(movies);
+});
+
 app.use(notFound);
 app.use(serError);
 
